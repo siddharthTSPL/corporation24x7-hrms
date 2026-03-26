951a4fcc280ea5873aabcf1b63e32fd8154331f8
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-03-26 16:12:28 +0530

    error handling done in manager controller

--- Backend/routes/managerroutes.js
+++ Backend/routes/managerroutes.js
@@ -1,43 +1,108 @@
-const express=require('express');
-const managerrouter=express.Router();
-const managercontroller=require('../controllers/manager.controller');
-const managermiddleware=require('../middleware/auth/manager.middleware');
+const express = require("express");
+const managerrouter = express.Router();
+const managercontroller = require("../controllers/manager.controller");
+const managermiddleware = require("../middleware/auth/manager.middleware");
+const asyncHandler = require("../middleware/errorhandling/asynchandler");
 
-managerrouter.get("/verify/:token", managercontroller.verifyManagerEmail);
-managerrouter.post('/login',managercontroller.managerlogin);
+managerrouter.get(
+  "/verify/:token",
+  asyncHandler(managercontroller.verifyManagerEmail),
+);
+managerrouter.post("/login", asyncHandler(managercontroller.managerlogin));
 
-managerrouter.post('/logout',managermiddleware,managercontroller.managerlogout);
+managerrouter.post(
+  "/logout",
+  managermiddleware,
+  asyncHandler(managercontroller.managerlogout),
+);
 
-managerrouter.get( "/change-password", managercontroller.showPasswordPage);
-managerrouter.post('/firstloginpasswordchange',managercontroller.managerFirstLoginPasswordChange);
+managerrouter.get("/change-password", managercontroller.showPasswordPage);
+managerrouter.post(
+  "/firstloginpasswordchange",
+  asyncHandler(managercontroller.managerFirstLoginPasswordChange),
+);
 
-managerrouter.put('/updatepassword', managermiddleware,managercontroller.managerUpdatePassword);
-managerrouter.get('/userunderme', managermiddleware,managercontroller.userunderme);
-managerrouter.get('/viewallleaves',managermiddleware,managercontroller.viewallleaves);
+managerrouter.put(
+  "/updatepassword",
+  managermiddleware,
+  asyncHandler(managercontroller.managerUpdatePassword),
+);
 
-managerrouter.post('/acceptleaverequest',managermiddleware,managercontroller.acceptleaverequest);
-managerrouter.post('/rejectleaverequest',managermiddleware,managercontroller.rejectleaverequest);
+managerrouter.get(
+  "/userunderme",
+  managermiddleware,
+  asyncHandler(managercontroller.userunderme),
+);
+managerrouter.get(
+  "/viewallleaves",
+  managermiddleware,
+  asyncHandler(managercontroller.viewallleaves),
+);
 
-managerrouter.post('/forwardtoadmin', managermiddleware,managercontroller.forwardedtoadmin);
+managerrouter.post(
+  "/acceptleaverequest",
+  managermiddleware,
+  asyncHandler(managercontroller.acceptleaverequest),
+);
+managerrouter.post(
+  "/rejectleaverequest",
+  managermiddleware,
+  asyncHandler(managercontroller.rejectleaverequest),
+);
 
-managerrouter.get("/employeedocuments/:employeeId", managermiddleware, managercontroller.viewEmployeeDocuments);
+managerrouter.post(
+  "/forwardtoadmin",
+  managermiddleware,
+  asyncHandler(managercontroller.forwardedtoadmin),
+);
 
+managerrouter.get(
+  "/employeedocuments/:employeeId",
+  managermiddleware,
+  managercontroller.viewEmployeeDocuments,
+);
 
-managerrouter.get('/showannouncements',managermiddleware,managercontroller.showannouncements);
+managerrouter.get(
+  "/showannouncements",
+  managermiddleware,
+  asyncHandler(managercontroller.showannouncements),
+);
 
+managerrouter.post(
+  "/forgetpassword",
+  asyncHandler(managercontroller.forgetpasswordloginbyotp),
+);
+managerrouter.post(
+  "/verifyMotp",
+  asyncHandler(managercontroller.verifyManagerOtp),
+);
+managerrouter.get(
+  "/showPasswordPageotp",
+  managercontroller.showPasswordPageotp,
+);
+managerrouter.post(
+  "/resetManagerPassword",
+  asyncHandler(managercontroller.resetManagerPassword),
+);
+managerrouter.get(
+  "/getmyleaves",
+  managermiddleware,
+  asyncHandler(managercontroller.getmyleaves),
+);
+managerrouter.post(
+  "/applyleavem",
+  managermiddleware,
+  asyncHandler(managercontroller.applyleavem),
+);
+managerrouter.post(
+  "/reviewtoemployee",
+  managermiddleware,
+  asyncHandler(managercontroller.reviewtoemployee),
+);
+managerrouter.get(
+  "/getme",
+  managermiddleware,
+  asyncHandler(managercontroller.getme),
+);
 
-managerrouter.get("/allemployeewhounderme", managermiddleware, managercontroller.viewallemployeewhounderme); 
-
-managerrouter.post('/forgetpassword',managercontroller.forgetpasswordloginbyotp);
-managerrouter.post('/verifyMotp',managercontroller.verifyManagerOtp);
-managerrouter.get('/showPasswordPageotp',managercontroller.showPasswordPageotp);
-managerrouter.post('/resetManagerPassword',managercontroller.resetManagerPassword);
-managerrouter.get('/getmyleaves',managermiddleware,managercontroller.getmyleaves);
-managerrouter.post('/applyleavem',managermiddleware,managercontroller.applyleavem);
-managerrouter.post('/reviewtoemployee',managermiddleware,managercontroller.reviewtoemployee);
-managerrouter.get('/getme',managermiddleware,managercontroller.getme);
-
-
-
-
-module.exports=managerrouter;
+module.exports = managerrouter;
