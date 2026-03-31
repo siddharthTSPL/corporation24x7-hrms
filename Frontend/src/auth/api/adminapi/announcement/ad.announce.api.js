1ae32be9b4cbfe70654a13add607fe2ed75149d5
nandini gupta <153939353+nandinigupt@users.noreply.github.com>
2026-03-31 17:07:30 +0530

    changes till 31/03/2026

--- Frontend/src/auth/api/adminapi/announcement/ad.announce.api.js
+++ Frontend/src/auth/api/adminapi/announcement/ad.announce.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/admin',
+  baseURL: 'http://localhost:5000/api/admin',
   withCredentials: true,
 });
--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-
+import api from "../../api";
 export default function Login() {
   const navigate = useNavigate();
 
@@ -25,18 +25,41 @@ export default function Login() {
   };
 
   const validateLogin = () => {
-    let newErrors = {};
-    if (!form.email) newErrors.email = "Email required";
-    if (!form.password) newErrors.password = "Password required";
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
+  let newErrors = {};
 
-  const handleLogin = () => {
-    if (validateLogin()) {
-      navigate("/dashboard");
-    }
-  };
+  if (!form.email) newErrors.email = "Email required";
+  if (!form.password) newErrors.password = "Password required";
+
+  setErrors(newErrors);
+  return Object.keys(newErrors).length === 0;
+};
+
+  const handleLogin = async () => {
+  if (!validateLogin()) return;
+
+  try {
+    const res = await api.post("", {
+  identifier: form.email,   
+  password: form.password
+});
+
+    console.log(res.data);
+
+    alert("Login successful ✅");
+
+    // store data (optional)
+    localStorage.setItem("admin", JSON.stringify(res.data.admin));
+
+    navigate("/dashboard");
+
+  } catch (error) {
+    console.log(error);
+
+    setErrors({
+      api: error.response?.data?.message || "Login failed"
+    });
+  }
+};
 
   const handleSendOtp = () => {
     if (!form.email) {
@@ -69,7 +92,7 @@ export default function Login() {
 
         {/* LEFT SIDE */}
         <div className="w-full md:w-1/2 p-8">
-          <img src="/logo.jpeg" alt="logo" className="w-28 mb-6" />
+          <img src="/logo1.jpeg" alt="logo" className="w-28 mb-6" />
 
           {step === "login" && (
             <>
