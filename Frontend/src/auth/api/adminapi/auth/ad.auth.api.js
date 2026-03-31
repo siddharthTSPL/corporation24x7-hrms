f1f9ab72b2b4d3bb09a3e116d15ab74af0222b1c
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-03-31 17:42:44 +0530

    api setup done in frontend

--- Frontend/src/auth/api/adminapi/auth/ad.auth.api.js
+++ Frontend/src/auth/api/adminapi/auth/ad.auth.api.js
@@ -1,6 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api= axios.create({
-  baseURL: 'http://localhost:8080/api/admin',
+const api = axios.create({
+  baseURL: "http://localhost:8080/api/admin",
   withCredentials: true,
 });
+
+export const registerAdmin = async (data) => {
+  const res = await api.post("/register", data);
+  return res.data;
+};
+
+export const verifyAdmin = async (token) => {
+  const res = await api.get(`/verify/${token}`);
+  return res.data;
+};
+
+export const loginAdmin = async (data) => {
+  const res = await api.post("/login", data);
+  return res.data;
+};
+
+export const logoutAdmin = async () => {
+  const res = await api.post("/logout");
+  return res.data;
+};
+
+export const getMeAdmin = async () => {
+  const res = await api.get("/getme");
+  return res.data;
+};
+
+export const addManager = async (data) => {
+  const res = await api.post("/addmanager", data);
+  return res.data;
+};
+
+export const addEmployee = async (data) => {
+  const res = await api.post("/addemployee", data);
+  return res.data;
+};
--- Frontend/src/auth/api/employeeapi/leave/em.leave.api.js
+++ Frontend/src/auth/api/employeeapi/leave/em.leave.api.js
@@ -4,3 +4,18 @@ const api= axios.create({
   baseURL: 'http://localhost:8080/api/employee',
   withCredentials: true,
 });
+
+export const applyLeave = async (data) => {
+  const res = await api.post("/applyleave", data);
+  return res.data;
+};
+
+export const getLeaveResult = async (id) => {
+  const res = await api.get(`/resultofleaverequest/${id}`);
+  return res.data;
+};
+
+export const getAllLeaves = async () => {
+  const res = await api.get("/getallleave");
+  return res.data;
+};
