d364ec6a681ef865f17944835e44fa901a27017b
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-03-31 12:56:28 +0530

    admin done

--- Backend/Models/Admin.model.js
+++ Backend/Models/Admin.model.js
@@ -1,38 +1,45 @@
-const moongose=require('mongoose');
-const bcrypt=require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const adminSchema=new moongose.Schema({
-     username:{
-          type:String,
-          required:[true,'Username is required']
-     },
-     password:{
-          type:String,
-          required:[true,'Password is required']
-     },
-     status:{
-          type:String,
-          enum:['active','inactive'],
-          default:'active'
-     },
-     role:{
-          type:String,
-          default:'admin'
-     },
-     email:{
-          type:String,
-          required:[true,'Email is required'],
-          unique:[true,'Email already exists']
-     }
-})
-adminSchema.pre('save',async function(next){
-     if(!this.isModified('password')) return next();
-     this.password=await bcrypt.hash(this.password,10);
-     next();
-})
+const adminSchema = new mongoose.Schema({
+  organisation_name: {
+    type: String,
+    required: [true, "Organisation name is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+  },
+  role: {
+    type: String,
+    default: "admin",
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-adminSchema.methods.isValidPassword=async function(password){
-     return await bcrypt.compare(password,this.password);
-}
-const Adminmodel=moongose.model('Admin',adminSchema);
-module.exports=Adminmodel
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+adminSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const Adminmodel = mongoose.model("Admin", adminSchema);
+module.exports = Adminmodel;
