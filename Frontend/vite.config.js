8eec520a7a01419a59609ac023675120ee6cc54d
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-04-10 23:51:56 +0530

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+
   assetsInclude: ["**/*.json"],
 
-})
+  build: {
+    sourcemap: false, 
+    minify: "terser", 
+
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+        drop_debugger: true,
+      },
+    },
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+
+  server: {
+    host: true,
+    port: 5173,
+  },
+});
