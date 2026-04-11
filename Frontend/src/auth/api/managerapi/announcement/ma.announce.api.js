aeeec92c655bdfdbd47d3deef379f441b114e51c
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-04-12 00:12:07 +0530

    manager dashboard and annonuncement done

--- Frontend/src/auth/api/managerapi/announcement/ma.announce.api.js
+++ Frontend/src/auth/api/managerapi/announcement/ma.announce.api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/manager',
+  baseURL: 'http://localhost:5000/manager',
   withCredentials: true,
 });
 
 export const getManagerAnnouncements = async () => {
   const res = await api.get("/showannouncements");
+  return res.data;
+};
+export  const particularAnnouncement = async (id) => {
+  const res = await api.get(`/showannouncement/${id}`);
   return res.data;
 };
