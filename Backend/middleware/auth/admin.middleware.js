1ff31cc3b9ce08b7505791d3e932becdac88c76c
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-05-13 13:12:40 +0530

    update code

--- Backend/middleware/auth/admin.middleware.js
+++ Backend/middleware/auth/admin.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const AdminModel = require("../Models/Admin.model");
+const AdminModel = require("../../Models/Admin.model");
 
 const adminAuth = async (req, res, next) => {
   try {
