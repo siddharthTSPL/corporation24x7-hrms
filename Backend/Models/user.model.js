84e299ab3c09f620bd1baf1da34b062072aa91f2
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-05-18 15:52:01 +0530

    .

--- Backend/Models/user.model.js
+++ Backend/Models/user.model.js
@@ -62,7 +62,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
-      select: false,
+      // select: false,
     },
 
     personal_contact: {
