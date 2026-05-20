4f8c5cdbd2ee8a69525c8135319cb94b11829f8c
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-05-20 14:04:25 +0530

    .

--- Frontend/src/auth/api/employeeapi/auth/em.auth.api.js
+++ Frontend/src/auth/api/employeeapi/auth/em.auth.api.js
@@ -27,7 +27,7 @@ export const firstLoginResetPassword = async (data) => {
 };
 
 export const updatePassword = async (data) => {
-  const res = await api.post("user/changepassword", data);
+  const res = await api.put("user/changepassword", data);
   return res.data;
 };
 
