406db91c0bf525884b6a1fda7140574e4e3846ea
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-06-05 18:23:52 +0530

    .

--- Backend/automatic/uidgeneration.js
+++ Backend/automatic/uidgeneration.js
@@ -1,8 +1,12 @@
 const UidCounter = require("../Models/UIDmodel.model");
 
-const generateUID = async (department) => {
+const generateUID = async (department, organisation_id) => {
+  if (!organisation_id) {
+    throw new Error("organisation_id is required to generate a UID");
+  }
+
   const counter = await UidCounter.findOneAndUpdate(
-    { department },
+    { department, organisation_id },
     { $inc: { lastNumber: 1 } },
     { new: true, upsert: true }
   );
