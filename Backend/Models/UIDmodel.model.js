a515a6bad51e428e39dd800ac286065f92f28e20
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-06-05 18:23:05 +0530

    .

--- Backend/Models/UIDmodel.model.js
+++ Backend/Models/UIDmodel.model.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
 const uidCounterSchema = new mongoose.Schema({
-     department:{
-          type:String,
-          enum:["MGMT","OPR","BPO","HR","ENG"],
-          required:true,
-          unique:true
-     },
-     lastNumber:{
-          type:Number,
-          default:0
-     }
+  organisation_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SuperAdmin",
+    required: true,
+  },
+  department: {
+    type: String,
+    enum: ["MGMT", "OPR", "BPO", "HR", "ENG"],
+    required: true,
+  },
+  lastNumber: {
+    type: Number,
+    default: 0,
+  },
 });
+
+
+uidCounterSchema.index({ organisation_id: 1, department: 1 }, { unique: true });
 
 module.exports = mongoose.model("UidCounter", uidCounterSchema);
