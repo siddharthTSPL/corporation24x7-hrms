112e8dd73d1987d12b329a62fae7cf41fa93852c
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-06-14 11:04:55 +0530

    fixed

--- Backend/routes/userroutes.js
+++ Backend/routes/userroutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const userrouter = express.Router();
 const asyncHandler = require("../middleware/errorhandling/asynchandler");
 const employeemiddleware = require("../middleware/auth/employee.middleware");
+const checkPermission = require("../middleware/auth/Checkpermission.middleware");
+const multer = require("multer");
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const {
   verifyUserEmail,
@@ -28,93 +32,49 @@ const {
   getOrgInfo,
   firstLoginPasswordChange,
   showPasswordPage,
-  sendPasswordSetupLink
+  sendPasswordSetupLink,
 } = require("../controllers/user.controller");
+
+const {
+  uploadDocument,
+  getDocuments,
+  editDocument,
+  deleteDocument,
+} = require("../controllers/uploaddocument.controller");
 
 userrouter.get("/verify/:token", asyncHandler(verifyUserEmail));
 userrouter.post("/login", asyncHandler(userlogin));
 userrouter.post("/forgetpassword", asyncHandler(forgetpassword));
 userrouter.post("/verifyotp", asyncHandler(verifyOtp));
 userrouter.post("/resetpassword", asyncHandler(resetpassword));
+userrouter.get("/change-password", showPasswordPage);
+userrouter.post("/firstloginpasswordchange", asyncHandler(firstLoginPasswordChange));
 
 userrouter.post("/logout", employeemiddleware, asyncHandler(userlogout));
 userrouter.get("/getme", employeemiddleware, asyncHandler(getme));
 userrouter.put("/updateprofile", employeemiddleware, asyncHandler(editprofile));
-userrouter.put(
-  "/changepassword",
-  employeemiddleware,
-  asyncHandler(changepassword),
-);
+userrouter.put("/changepassword", employeemiddleware, asyncHandler(changepassword));
+userrouter.get("/getOrgInfo", employeemiddleware, asyncHandler(getOrgInfo));
+userrouter.post("/sendPasswordSetupLink", employeemiddleware, asyncHandler(sendPasswordSetupLink));
 
 userrouter.post("/applyleave", employeemiddleware, asyncHandler(applyleave));
 userrouter.put("/editleave/:id", employeemiddleware, asyncHandler(editleave));
-userrouter.delete(
-  "/deleteleave/:id",
-  employeemiddleware,
-  asyncHandler(deleteleave),
-);
+userrouter.delete("/deleteleave/:id", employeemiddleware, asyncHandler(deleteleave));
 userrouter.get("/getallleave", employeemiddleware, asyncHandler(getallleave));
-userrouter.get(
-  "/getallleavehistory",
-  employeemiddleware,
-  asyncHandler(getallleavehistory),
-);
+userrouter.get("/getallleavehistory", employeemiddleware, asyncHandler(getallleavehistory));
+userrouter.get("/getattendance", employeemiddleware, asyncHandler(getattendance));
 
-userrouter.get(
-  "/showannouncements",
-  employeemiddleware,
-  asyncHandler(showannouncements),
-);
-userrouter.get(
-  "/showannouncement/:id",
-  employeemiddleware,
-  asyncHandler(showparticularannouncement),
-);
+userrouter.get("/showannouncements", employeemiddleware, checkPermission("announcements.can_view_announcements"), asyncHandler(showannouncements));
+userrouter.get("/showannouncement/:id", employeemiddleware, checkPermission("announcements.can_view_announcements"), asyncHandler(showparticularannouncement));
 
-userrouter.get(
-  "/getattendance",
-  employeemiddleware,
-  asyncHandler(getattendance),
-);
+userrouter.post("/upload", employeemiddleware, checkPermission("documents.can_upload_documents"), upload.single("file"), uploadDocument);
+userrouter.get("/documents", employeemiddleware, checkPermission("documents.can_upload_documents"), getDocuments);
+userrouter.put("/documents/:id", employeemiddleware, checkPermission("documents.can_upload_documents"), upload.single("file"), editDocument);
+userrouter.delete("/documents/:id", employeemiddleware, checkPermission("documents.can_upload_documents"), deleteDocument);
 
-userrouter.post(
-  "/submitTicket",
-  employeemiddleware,
-  asyncHandler(employeeSubmitTicket),
-);
-userrouter.get(
-  "/getMyTickets",
-  employeemiddleware,
-  asyncHandler(employeeGetMyTickets),
-);
-userrouter.post(
-  "/rateTicket",
-  employeemiddleware,
-  asyncHandler(employeeRateTicket),
-);
-
-userrouter.get(
-  "/getTicketDetail/:ticketNumber",
-  employeemiddleware,
-  asyncHandler(employeeGetTicketDetail),
-);
-
-userrouter.get(
-  "/change-password",
-  showPasswordPage
-);
-
-userrouter.post(
-  "/firstloginpasswordchange",
-  asyncHandler(firstLoginPasswordChange)
-);
-
-userrouter.post(
-  "/sendPasswordSetupLink",
-  employeemiddleware,   // <-- add middleware here
-  asyncHandler(sendPasswordSetupLink)
-);
-
-userrouter.get("/getOrgInfo", employeemiddleware, asyncHandler(getOrgInfo));
+userrouter.post("/submitTicket", employeemiddleware, checkPermission("tickets.can_raise_ticket"), asyncHandler(employeeSubmitTicket));
+userrouter.get("/getMyTickets", employeemiddleware, checkPermission("tickets.can_raise_ticket"), asyncHandler(employeeGetMyTickets));
+userrouter.post("/rateTicket", employeemiddleware, checkPermission("tickets.can_rate_ticket"), asyncHandler(employeeRateTicket));
+userrouter.get("/getTicketDetail/:ticketNumber", employeemiddleware, checkPermission("tickets.can_raise_ticket"), asyncHandler(employeeGetTicketDetail));
 
 module.exports = userrouter;
