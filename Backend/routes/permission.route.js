dce35eb4aa041f216fbd93d8eabf2e1dbcf4b994
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-06-15 12:11:46 +0530

    fixed

--- Backend/routes/permission.route.js
+++ Backend/routes/permission.route.js
@@ -16,12 +16,12 @@ const employeemiddleware = require("../middleware/auth/employee.middleware");
 router.post("/assign/superadmin", superAdminAuth, createOrUpdatePermission);
 router.post("/assign/admin", adminauthmiddleware, createOrUpdatePermission);
 
-router.get("/:user_model/:user_id", superAdminAuth, getPermissions);
-router.get("/admin/:user_model/:user_id", adminauthmiddleware, getPermissions);
-
 router.get("/me/admin", adminauthmiddleware, getMyPermissions);
 router.get("/me/manager", managermiddleware, getMyPermissions);
 router.get("/me/employee", employeemiddleware, getMyPermissions);
+
+router.get("/:user_model/:user_id", superAdminAuth, getPermissions);
+router.get("/admin/:user_model/:user_id", adminauthmiddleware, getPermissions);
 
 router.delete("/superadmin/:user_model/:user_id", superAdminAuth, deletePermission);
 router.delete("/admin/:user_model/:user_id", adminauthmiddleware, deletePermission);
