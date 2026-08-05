aa529403ee7885ca3b5d2901fd7010dbfe194446
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-08-05 12:44:30 +0530

    Add reportingManager to useGetMeManager query response

--- Frontend/src/auth/server-state/manager/managerauth/managerauth.hook.js
+++ Frontend/src/auth/server-state/manager/managerauth/managerauth.hook.js
@@ -60,7 +60,7 @@ export const useGetMeManager = () => {
       manager: data.manager,
       leavebalance: data.leavebalance,
       reviews: data.reviews,
-      reportingManager: data.reportingManager,   // ← ye line add karni hai
+      reportingManager: data.reportingManager,   
     }),
   });
 };
