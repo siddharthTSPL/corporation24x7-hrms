f6c3f7efcf8a79a1f3d82e80a1f6c8dcaf09b72b
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-08-07 13:40:40 +0530

    Add Notification routes for managing user notifications

---
+++ Backend/routes/Notification.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notificationRouter = express.Router();
+
+const asyncHandler = require("../middleware/errorhandling/asynchandler");
+const anyRoleAuth = require("../middleware/auth/anyRole.middleware");
+
+const {
+  getMyNotifications,
+  getUnreadCount,
+  markAsRead,
+  markAllAsRead,
+  deleteNotification,
+  clearReadNotifications,
+} = require("../controllers/notification.controller");
+
+notificationRouter.get("/", anyRoleAuth, asyncHandler(getMyNotifications));
+notificationRouter.get("/unread-count", anyRoleAuth, asyncHandler(getUnreadCount));
+notificationRouter.patch("/read-all", anyRoleAuth, asyncHandler(markAllAsRead));
+notificationRouter.patch("/:id/read", anyRoleAuth, asyncHandler(markAsRead));
+notificationRouter.delete("/clear-read", anyRoleAuth, asyncHandler(clearReadNotifications));
+notificationRouter.delete("/:id", anyRoleAuth, asyncHandler(deleteNotification));
+
+module.exports = notificationRouter;
