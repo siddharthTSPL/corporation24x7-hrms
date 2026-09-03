687be3649e38d7d928435ef3043342583d195eed
pawan kumar <pawankumarruhela2004@gmail.com>
2026-09-03 17:05:08 +0530

    .

--- Frontend/src/pages/announcement/landingpage.jsx
+++ Frontend/src/pages/announcement/landingpage.jsx
@@ -923,10 +923,11 @@ function Pricing() {
     </section>
   )
 } 
+
 function Testimonials() {
   const [startIndex, setStartIndex] = useState(0)
   const testimonials = [
-    { quote: 'TorchX Talent has completely transformed our hiring process. The AI recruitment feature helps us find the right talent faster and with better accuracy.', name: 'Alexa', role: 'HR Manager', initials: 'AL' },
+    { quote: 'TorchX Talent has completely transformed our hiring process. The AI recruitment feature helps us find the right talent faster and with better accuracy.', name: 'KK Oberoi', role: 'HR Manager', initials: 'AL' },
     { quote: 'The employee portal is a game changer! Our team loves the easy access to documents, requests, and updates all in one place.', name: 'Anaya Varma', role: 'HR Director',  initials: 'AV' },
     { quote: 'Performance reviews are now simple, transparent, and data-driven. TorchX Talent helps us build a culture of continuous feedback and growth.', name: 'Rohan Sharma', role: 'People Operations Lead' , initials: 'RS' },
     { quote: 'TorchX Talent has significantly improved our workforce management. From onboarding to performance tracking, everything is streamlined and easy to manage.', name: 'Karan Malhotra', role: 'Head of Human Resources',  initials: 'KM' },
