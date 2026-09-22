dba87b72bca8cc8ca19bfaaeeb74be7704e54d6c
pawan kumar <pawankumarruhela2004@gmail.com>
2026-09-22 18:51:29 +0530

    .

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -4023,8 +4023,7 @@ const getAttendanceOverview = async (req, res, next) => {
 
     const [managers, employees] = await Promise.all([
       teamManagerIds.length
-<<<<<<< HEAD
-  ? Managermodel.find({ organisation_id, working_status: "working", _id: { $in: teamManagerIds } })
+? Managermodel.find({ organisation_id, working_status: "working", _id: { $in: teamManagerIds } })
             .select("empid f_name l_name work_email role designation department office_location profile_image reporting_manager reporting_manager_model")
             .populate({ path: "reporting_manager", select: "f_name l_name empid" })
             .lean()
@@ -4032,26 +4031,6 @@ const getAttendanceOverview = async (req, res, next) => {
      teamManagerIds.length
   ? Usermodel.find({ organisation_id, working_status: "working", Under_manager: { $in: teamManagerIds } })
             .select("empid f_name l_name work_email role designation department office_location profile_image Under_manager")
-=======
-        ? Managermodel.find({ organisation_id, _id: { $in: teamManagerIds } })
-            .select(
-              "empid f_name l_name work_email role designation department office_location profile_image reporting_manager reporting_manager_model"
-            )
-            .populate({
-              path: "reporting_manager",
-              select: "f_name l_name empid",
-            })
-            .lean()
-        : [],
-      teamManagerIds.length
-        ? Usermodel.find({
-            organisation_id,
-            Under_manager: { $in: teamManagerIds },
-          })
-            .select(
-              "empid f_name l_name work_email role designation department office_location profile_image Under_manager"
-            )
->>>>>>> 0c7ecd5754888576d4e9ceb531d9e459b11d66a0
             .populate({ path: "Under_manager", select: "f_name l_name empid" })
             .lean()
         : [],
